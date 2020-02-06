46f5c29f9ae72736e82400c3ba4eed28137c7311
blacktermi <blacktermi@blacktermi.blacktermi>
2020-02-06 12:28:25 +0000

    premier commit

---
+++ index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="fr_FR" class="no-js">
+
+<head>
+	<!-- Primary Meta Tags -->
+	<title>Afro Tech AGENCE</title>
+	<meta name="title" content="Afro Tech">
+	<meta name="description" content="Agence de communication 360 spécialisé dans le déploiement de solutions digitale pour entreprise et particulier">
+	<meta name="author" content="Afro Tech">
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="/img/fav.png">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://web.facebook.com/CodeWebagence/">
+	<meta property="og:title" content="AGENCE Afro Tech">
+	<meta property="og:description" content="Agence de communication 360 spécialisé dans le déploiement de solutions digitale pour entreprise et particulier">
+	<meta property="og:image" content=">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://metatags.io/">
+	<meta property="twitter:title" content="AGENCE Afro Tech">
+	<meta property="twitter:description" content="Agence de communication 360 spécialisé dans le déploiement de solutions digitale pour entreprise et particulier">
+	<meta property="twitter:image" content=">
+	<!-- Meta mot clés -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Titre du site-->
+	<title>Afro Tech</title>
+	<link  rel = "icône de raccourci"  href = "favicon.ico"  type = "image / x-icon" > 
+	<link  rel = "icon"  href = "favicon.ico"  type = "image / x-icon" >
+
+	<!--	CSS	============================================= -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css">
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="#" type="image/png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/fontawesome-all.css">
+	<link rel="stylesheet" type="text/css" href="css/icofont.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.lineProgressbar.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.css">
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="css/responsiveslides.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- Facebook Messenger Live chat -->
+</head>
+
+<body>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+	 attribution=setup_tool
+	 page_id="189880258612845"
+	 theme_color="#2ECC71"
+	 logged_in_greeting="Bienvenue ! Comment pouvons-nous vous aider ?"
+	 logged_out_greeting="Bienvenue ! Comment pouvons-nous vous aider ?"
+	 >
+</div>
+
+
+<div class="top-header hidden-xs hidden-sm">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="top-item-left">
+						<ul>
+							<li><a href="#"><i class="fa fa-envelope"></i> codeweb.services@gmail.com</a></li>
+							<li><a href="#"><i class="fa fa-phone"></i> +225 88 51 67 55</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="top-item-right">
+						<ul>
+							<li><a href="https://web.facebook.com/CodeWebagence/"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="https://twitter.com/CodeWeb4"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="https://linkedin.com/company/afro-tech-agence"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<header id="pageHeader" class="header navbar">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar icon-bar-last"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">
+					<img src="img/logocw.svg" alt="logo_codeweb" style="width: 80px;"/>
+				</a>
+			</div>
+			<nav id="myNavbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Accueil</a></li>
+					<li><a href="quinoussomme.html">Qui Sommes-Nous</a></li>
+                    <li><a href="creationweb.html">Création de site Web</a></li>
+                    <li><a href="graphism.html">Graphisme</a></li>
+					<li><a href="#contacts">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<div id="accueil" class="main-header bg-opacity">
+		<div id="particles-js"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="main-header-text">
+						<h1>Propulsez votre communication</h1>
+						<h3>Avec nos solutions de <span class="element"></span></h3>
+						<a class="btn js-scroll-trigger" style="background: #f05f40; border-radius: 5px 5px 5px 5px;" href="#services">En savoir plus</a>					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	<section id="services" class="pt-120 pb-90 services">
+		<div class="container">
+			<div class="row">
+				<div class="section-title">
+					<div class="main-title">
+						<h1>NOS SERVICES</h1>
+						<h4>NOTRE EXPERTISE</h4>
+					</div>
+					<p class="col-md-8 col-md-offset-2">
+						Etre présent sur internet est devenu incontournable, mais par où commencer ? Afro Tech vous accompagne de A à Z dans vos projets web et mobile. Nous réalisons vos projets sur-mesure, avec le plus grand soin et avec un processus bien rigoureux. Osez l'aventure !
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-sm-6">
+					<div class="single-service text-center">
+						<div class="icon">
+							<img src="img/coding.svg" data-rjs="2" alt="">
+						</div>
+
+						<div class="content">
+							<h4>Développement Web</h4>
+							<p>De la conception de sites web professionnels à l'hébergement en passant par l'intégration des dernières technologies et tendances, nous vous accompagnons de bout en bout. Réinventez le web à l'aide de nos experts !</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6">
+					<div class="single-service text-center">
+						<div class="icon">
+							<img src="img/analytics.svg" data-rjs="2" alt="">
+						</div>
+
+						<div class="content">
+							<h4>Marketing Digital</h4>
+							<p>Grâce au digital, nous vous offrons l'opportunité d'accroître considérablement le nombre de vos
+							prospects, et augmenter vos revenus</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6">
+					<div class="single-service text-center">
+						<div class="icon">
+							<img src="img/art-design.svg" data-rjs="2" alt="">
+						</div>
+
+						<div class="content">
+							<h4>Graphisme</h4>
+							<p>La puissance de la création digitale à votre service. Séduisez par des affiches et des logos au design innovant, créez-vous une identité visuelle forte. Contentez-vous de rêver, nous créons !</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- FIN SECTION SERVICES -->
+
+	<!-- SECTION CLIENTS -->
+	<section id="client" class="padding-60;" style="padding-top: 200px; padding-bottom: 200px;">
+		<div class="container">
+			<div class="row">
+				<div class="section-title">
+					<div class="main-title">
+						<h1>NOS R&Eacute;F&Eacute;RENCES TECHNOLOGIQUES</h1>
+						<h4>Projet realisé</h4>
+					</div>
+			</div>
+			<div class="row">
+				<div id="client-caro" class="clients-carousel  owl-carousel">
+					<div class="item">
+						<a href="img/p1.JPG"><img src="img/p1.JPG" class="img-responsive" alt=""></a>
+					</div>
+					<div class="item">
+						<a href="img/p2.JPG"><img src="img/p2.JPG" class="img-responsive" alt=""></a>
+					</div>
+					<div class="item">
+						<a href="img/3.jpg"><img src="img/3.jpg" class="img-responsive" alt=""></a>
+					</div>
+					<div class="item">
+						<a href="img/p4.JPG"><img src="img/p4.JPG" class="img-responsive" alt=""></a>
+					</div>
+					<div class="item">
+						<a href="img/gh.jpg"><img src="img/gh.jpg" class="img-responsive" alt=""></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- FIN  SECTION CLIENT -->
+
+
+	<!-- SECTION REJOIGNEZ-NOUS -->
+	<section id="carriere" class="padding-60 bg-request bg-opacity">
+		<div class="container">
+			<div class="row">
+				<div class="work-with text-center">
+					<h4>ENVIE DE FAIRE PARTIR DE L'&Eacute;QUIPE?</h4>
+					<p>Vous souhaitez participer à une aventure au delà des limites</p>
+					<a href="#" style="background: #f05f40; border-radius: 5px 5px 5px 5px;">REJOIGNEZ NOUS</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- FIN SECTION REJOIGNEZ-NOUS-->
+
+	<!-- SECTION CONTACTS-->
+
+	<!-- FIN SECTION CONTACTS-->
+	<section id="contacts" class="padding-60">
+		<div class="container">
+			<div class="row">
+				<div class="section-title">
+					<div class="main-title">
+						<h1>QU'ATTENDEZ-VOUS?</h1>
+						<h4>CONTACTEZ-NOUS</h4>
+					</div>
+					<p class="col-md-8 col-md-offset-2">Besoin d'un service particulier, nous sommes à votre entière disposition.
+						Vous avez juste à remplir le formulaire ci-dessous </p>
+				</div>
+				<div class="contact-form">
+					<form class="contact-form" action="contact.php" method="post">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<input type="text" class="form-control" name="nom" placeholder="Nom Complet / Nom de L'entreprise..." required="required">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<input type="email" class="form-control" name="email" placeholder="Adresse email..." required="required"/>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<input type="text" class="form-control" name="telephone" placeholder="Numéro de téléphone..." required="required" />
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<input type="text" class="form-control" name="objet" placeholder="Objet du Message..." required="required">
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<textarea class="form-control" name="message" placeholder="À vous de jouer..."></textarea>
+								</div>
+							</div>
+							<div class="col-md-12 text-center">
+								<button class="btn submit-btn" style="background: #f05f40; border-radius: 5px 5px 5px 5px;"><i class="fa fa-paper-plane"></i> ENVOYER</button>
+							</div>
+						</div>
+					</form>
+					<br/>
+                    <span id="notification"></span>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- PIED DE PAGE -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 footer-logo">
+					<a class="navbar-brand" href="#">
+						<img src="img/logocw.svg" alt="logo_codeweb" style="width: 80px;"/>
+					</a>
+					<p>Afro Tech est une Agence Web spécialisée dans le développement, l’exploitation et la maintenance de services web innovants. Notre mission consiste à aider votre entreprise à accroître son chiffre d’affaire en se servant du webmarketing et à vous aider en tant que particulier à avoir accès à des services en ligne qui vous faciliteront le quotidien.</p>
+					<ul>
+						<li><a href="https://web.facebook.com/CodeWebagence/"><i style="background: #f05f40;" class="fab fa-facebook-f"></i></a></li>
+						<li><a href="https://twitter.com/CodeWeb4"><i style="background: #f05f40;" class="fab fa-twitter"></i></a></li>
+						<li><a href="https://linkedin.com/company/afro-tech-agence"><i style="background: #f05f40;" class="fab fa-linkedin-in"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-md-6 footer-links" style="padding-left: 70px; padding-top: 85px;">
+					<h5 class="footer-title">Plan du site</h5>
+					<ul class="footer-link">
+						<li><a href="index.php"><i class="fas fa-angle-double-right"></i> ACCUEIL</a></li>
+						<li><a href="#services"><i class="fas fa-angle-double-right"></i> NOTRE EXPERTISE</a></li>
+						<li><a href="#"><i class="fas fa-angle-double-right"></i> PORTFOLIO</a></li>
+						<li><a href="#contacts"><i class="fas fa-angle-double-right"></i> CONTACTS</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 footer-news" style="padding-top: 85px;">
+					<h5 class="footer-title">Restez Informez</h5>
+					<p>Restez à l'écoute, nous nous rapprochons encore plus de vous</p>
+					<div class="newsletter">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Votre adresse email" aria-label="Search for..." autocomplete="off">
+							<span class="input-group-btn">
+                      	<button class="btn newsletter-btn" type="button"><i class="fa Example of paper-plane fa-paper-plane"></i></button>
+                    	</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<div class="footer-down">
+		<div class="container">
+			<div class="row text-center">
+				<div class="copyright-text">
+					<p>© Copyright 2019. Tous droits réservés. <a class="footer-team" href="#">Afro Tech</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- FIN PIED DE PAGE ==== -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.parallax-1.1.3.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/isotope.js"></script>
+	<script src="js/particles.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/responsiveslides.js"></script>
+	<script src="js/jquery.lineProgressbar.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/typed.js"></script>
+	<script src="js/jquery.counterup.min.js"></script>
+	<script src="js/masonry.pkgd.js"></script>
+	<script src="js/tweetie.js"></script>
+	<script src="js/main.js"></script>
+	<script>
+        /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+        particlesJS.load('particles-js', 'assets/particles.json', function() {
+            console.log('callback - particles.js config loaded');
+        });
+	</script>
+	<!-- Go to www.addthis.com/dashboard to customize your tools -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d3f28b9873e15af"></script>
+	<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v3.3'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+
+</body>
+
+</html>
